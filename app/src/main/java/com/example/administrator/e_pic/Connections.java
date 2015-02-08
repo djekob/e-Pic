921bc58b0163d8a7f3ad7d603182a6211ca391a7
djekob <jakob.biebuyck@gmail.com>
2015-02-08 11:30:23 +0100

    vs23

--- app/src/main/java/com/example/administrator/e_pic/Connections.java
+++ app/src/main/java/com/example/administrator/e_pic/Connections.java
@@ -19,15 +19,18 @@ import java.util.List;
 public class Connections {
 
     private static final String TAG_SUCCESS = "success";
+    private static final String URL_CREATE_USER = "http://unuzeleirstest.netau.net/create_user.php";
+    private static final String URL_ADD_SNEEZE = "http://unuzeleirstest.netau.net/add_sneeze.php";
+    public static final String NAAM_VAR_USER = "Username";
     //public static final int ADD_USER = 0;
     private ProgressDialog pDialog;
     private Context context;
-    private String prename, name, username, password;
+    private String prename = null, name = null, username = null, password = null, timestamp = null;
     private int age;
 
-    public Connections(Context context){
+    public Connections(Context context, String timestamp){
         this.context = context;
-
+        this.timestamp = timestamp;
         new CreateSneeze().execute();
     }
 
@@ -39,15 +42,14 @@ public class Connections {
         this.age = age;
         this.context = context;
 
-        CreateNewUser cnu = new CreateNewUser();
-        cnu.execute();
+        new CreateNewUser().execute();
     }
 
     /**
      * Background Async Task to Create new user
      * */
     class CreateNewUser extends AsyncTask<String, String, String> {
-        private static final String URL_CREATE_USER = "http://unuzeleirstest.netau.net/create_user.php";
+
 
         /**
          * Before starting background thread Show Progress Dialog
@@ -93,6 +95,7 @@ public class Connections {
                     // successfully created product
                     Intent i = new Intent(context, iSneezeActivity.class);
                     i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    i.putExtra(NAAM_VAR_USER, prename + " " + name);
                     context.startActivity(i);
 
                     // closing this screen
@@ -123,16 +126,58 @@ public class Connections {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
+            pDialog = new ProgressDialog(context);
+            pDialog.setMessage("Add sneeze..");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            pDialog.show();
         }
 
         @Override
-        protected String doInBackground(String... params) {
+        protected String doInBackground(String... args) {
+
+            // Building Parameters
+            List<NameValuePair> params = new ArrayList<>();
+            params.add(new BasicNameValuePair("Loginnaam", username));
+            //params.add(new BasicNameValuePair("Timestamp", timestamp));
+
+            // getting JSON Object
+            // Note that create product url accepts POST method
+            JSONParser jsonParser = new JSONParser();
+
+
+            JSONObject json = jsonParser.makeHttpRequest(URL_ADD_SNEEZE,
+                    "POST", params);
+
+            // check log cat fro response
+
+            // check for success tag
+            try {
+                int success = json.getInt(TAG_SUCCESS);
+
+                if (success == 1) {
+                    // successfully created product
+                    /*Intent i = new Intent(context, iSneezeActivity.class);
+                    i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    context.startActivity(i);*/
+
+                    // closing this screen
+
+
+                } else {
+                    // failed to create product
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
             return null;
         }
 
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
+            pDialog.dismiss();
         }
     }
 }
--- app/src/main/java/com/example/administrator/e_pic/iSneezeActivity.java
+++ app/src/main/java/com/example/administrator/e_pic/iSneezeActivity.java
@@ -1,12 +1,16 @@
 package com.example.administrator.e_pic;
 
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 
 public class iSneezeActivity extends ActionBarActivity {
@@ -14,18 +18,34 @@ public class iSneezeActivity extends ActionBarActivity {
     private TextView myNameTextView;
     private String myName;
     private Connections c;
+    private Button isneeze_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_i_sneeze);
 
+        myName = getIntent().getExtras().getString(Connections.NAAM_VAR_USER);
+
         myNameTextView = (TextView) findViewById(R.id.my_name_textview);
+        isneeze_button = (Button) findViewById(R.id.isneeze_button);
+
         //myname definieren
         myNameTextView.setText(myName);
 
+        isneeze_button.setOnClickListener(new SneezeClickListener());
+
     }
 
+    public class SneezeClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            String timestamp = Calendar.YEAR + "-" + Calendar.MONTH + "-" + Calendar.DAY_OF_MONTH + " " + Calendar.HOUR + "-" + Calendar.MINUTE + "-" + Calendar.SECOND;
+
+            new Connections(getApplicationContext(), timestamp);
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
