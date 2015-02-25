7bdcb79186e11e5462f2470939f08bf3cdcaae6a
Thomas De Letter <thomas.deletter@ugent.be>
2015-02-25 16:02:27 +0100

    notificaties verschoond

--- app/src/main/java/com/example/administrator/e_pic/GcmIntentService.java
+++ app/src/main/java/com/example/administrator/e_pic/GcmIntentService.java
@@ -23,8 +23,11 @@ import android.app.IntentService;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.v4.app.NotificationCompat;
@@ -47,10 +50,16 @@ public class GcmIntentService extends IntentService {
         super("GcmIntentService");
     }
     public static final String TAG = "GCM Demo";
+    private static final String TAG_RECEIVED_FROM = "From";
+    private static final String TAG_NOTIFICATION_TYPE = "NotificationType";
+    private static final int CODE_ADD_SNEEZE = 0;
+    private static final int CODE_FRIEND_REQUEST = 1;
 
     @Override
     protected void onHandleIntent(Intent intent) {
         Bundle extras = intent.getExtras();
+        String from = extras.getString(TAG_RECEIVED_FROM);
+        int notType = extras.getInt(TAG_NOTIFICATION_TYPE);
         GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
         // The getMessageType() intent parameter must be the intent you received
         // in your BroadcastReceiver.
@@ -63,9 +72,9 @@ public class GcmIntentService extends IntentService {
              * not interested in, or that you don't recognize.
              */
             if (GoogleCloudMessaging.MESSAGE_TYPE_SEND_ERROR.equals(messageType)) {
-                sendNotification("Send error: " + extras.toString());
+                //sendNotification("Send error: " + extras.toString());
             } else if (GoogleCloudMessaging.MESSAGE_TYPE_DELETED.equals(messageType)) {
-                sendNotification("Deleted messages on server: " + extras.toString());
+                //sendNotification("Deleted messages on server: " + extras.toString());
                 // If it's a regular GCM message, do some work.
             } else if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType)) {
                 // This loop represents the service doing some work.
@@ -79,7 +88,7 @@ public class GcmIntentService extends IntentService {
                 }*/
                 Log.i(TAG, "Completed work @ " + SystemClock.elapsedRealtime());
                 // Post notification of received message.
-                sendNotification("Jakob has sneezed");
+                sendNotification(from, notType);
                 Log.i(TAG, "Received: " + extras.toString());
             }
         }
@@ -90,26 +99,58 @@ public class GcmIntentService extends IntentService {
     // Put the message into a notification and post it.
     // This is just one simple example of what you might choose to do with
     // a GCM message.
-    private void sendNotification(String msg) {
+    private void sendNotification(String from, int notificationType) {
+        int requestID = (int) System.currentTimeMillis();
+
+
+
         mNotificationManager = (NotificationManager)
                 this.getSystemService(Context.NOTIFICATION_SERVICE);
 
-        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
-                new Intent(this, iSneezeActivity.class), 0);
+
+        Intent notificationIntent = null;
 
         long[] vibrate = {130, 100, 130, 100};
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
                         .setSmallIcon(R.drawable.sneeze_icon)
-                        .setContentTitle("iSneeze")
-                        .setStyle(new NotificationCompat.BigTextStyle()
-                                .bigText(msg))
-                        .setContentText(msg)
-                        .setTicker("A friend sneezed")
-                        .setVibrate(vibrate);
+                        .setContentTitle("iSneeze");
+        switch (notificationType){
+            case CODE_ADD_SNEEZE:
+                mBuilder.setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText(from + " heeft geniesd."))
+                        .setContentText(from + " heeft geniesd.")
+                        .setTicker(from + " heeft geniesd.");
+                notificationIntent = new Intent(getApplicationContext(), iSneezeActivity.class);
+
+                //contentIntent = PendingIntent.getActivity(this, 0,
+                  //      new Intent(this.getApplicationContext(), iSneezeActivity.class), 0);
+
+                break;
+            case CODE_FRIEND_REQUEST:
+                mBuilder.setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText(from + " heeft je een vriendschapsverzoek gestuurd."))
+                        .setContentText(from + " heeft je een vriendschapsverzoek gestuurd.")
+                        .setTicker(from + " heeft je een vriendschapsverzoek gestuurd.");
+                notificationIntent = new Intent(getApplicationContext(), FriendRequestsActivity.class);
+
+                //contentIntent = PendingIntent.getActivity(this, 0,
+                  //      new Intent(this.getApplicationContext(), iSneezeActivity.class), 0);
+                break;
+            default: return;
+        }
+        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        PendingIntent contentIntent = PendingIntent.getActivity(this, requestID,notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
 
         mBuilder.setAutoCancel(true);
+
+
+
         mBuilder.setContentIntent(contentIntent);
+        mBuilder.setVibrate(vibrate);
+
 
         mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
     }
